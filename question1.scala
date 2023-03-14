c7c638753e722d70d610562ce6348cb367f7d3e8
Sonali-Gudey <123619701+Sonali-Gudey@users.noreply.github.com>
2023-03-14 12:54:27 +0530

    Update question1.scala

--- question1.scala
+++ question1.scala
@@ -1,21 +1,25 @@
-import scala.collection.mutable.ArrayBuffer
-
-class question1 {
-    def bucketize(arr: Array[Double]): Array[String] = {
-        val buckets = ArrayBuffer[String]()
-        for (i <- arr) {
-            val bucket = f"${math.floor(i)}%.3f-${math.floor(i) + 0.049}%.3f"
-            buckets += bucket
+class question1() {
+    def bucketize(n: Double): Unit = {
+        var last_two_digits = (String.format("%.2f", Double.valueOf((n * 1000) % 100)))
+        print(f"Value : $n")
+        if (last_two_digits.toDouble >= 50) {
+            val left = n - (last_two_digits.toDouble / 1000) + 0.050
+            val right = n + (99 - last_two_digits.toDouble) / 1000
+            print(f" Bucket : $left%,.3f" + " - " + f"$right%,.3f\n")
+        } else {
+            val left = n - (last_two_digits.toDouble / 1000)
+            val right = n + (49 - last_two_digits.toDouble) / 1000
+            print(f" Bucket : $left%,.3f" + " - " + f"$right%,.3f\n")
         }
-        buckets.toArray
     }
 }
 
 object question1 {
     def main(args: Array[String]): Unit = {
         val obj = new question1()
-        val arr = Array(12.05, 12.03, 10.33, 11.45, 13.50)
-        val result = obj.bucketize(arr)
-        println(result.mkString("[", ", ", "]"))
+        val values = Array(12.05, 12.99, 10.33, 11.45, 13.50)
+        for (i <- values) {
+            obj.bucketize(i)
+        }
     }
 }
